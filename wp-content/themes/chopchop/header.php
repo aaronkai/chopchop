9d4c448779394a777cdfcb6849df38eac7e1c579
Aaron Hubbard <abhorrentaaron@gmail.com>
2013-01-01 14:26:45 -0800

    Updated wp-content/themes/chopchop/header.php

--- wp-content/themes/chopchop/header.php
+++ wp-content/themes/chopchop/header.php
@@ -26,7 +26,24 @@
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
 <!--[if lt IE 9]>
+  
+  <!--google analytics-->
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+  
+  <script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-37351862-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+  
 <![endif]-->
 <?php wp_head(); ?>
 </head>
